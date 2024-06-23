268cd7f437c79c685bcd8192422aa3ad1ae4b345
TheRABU <mdfazlerabbi114@gmail.com>
2024-06-23 20:49:50 +0600

    done a lot of things in homepage

---
+++ src/components/landingUI/FeaturedSectionCustom/CustomFeatured.jsx
@@ -0,0 +1,30 @@
+import studentImage from "../../landingUI/std.avif";
+import "./feature.css";
+const CustomFeatured = () => {
+  return (
+    <>
+      <div className="featured-div h-screen mt-0 py-30 px-6 lg:px-36 bg-fixed block lg:flex items-center justify-between">
+        <div className="w-full py-4 lg:py-0 md:w-2/4 lg:w-4/6">
+          <img
+            className="w-full h-full object-cover"
+            src={studentImage}
+            alt=""
+          />
+        </div>
+        <div className="flex-grow flex-col items-center px-16 py-10">
+          <div className="text-contents py-6">
+            <h2 className="text-xl text-white lg:text-5xl">
+              Our students are world wide
+            </h2>
+            <p className="text-md lg:text-xl text-gray-300 font-semibold">
+              Our students have been into industries all over the world. We
+              proudly make them our alumni.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CustomFeatured;
