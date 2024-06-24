41fda6511e14b3f81849ff7748ac3608306b123f
TheRABU <mdfazlerabbi114@gmail.com>
2024-06-24 21:07:02 +0600

    added a non found text in tabs page

--- src/pages/AllCoursespage/CourseTabCompo.jsx
+++ src/pages/AllCoursespage/CourseTabCompo.jsx
@@ -1,9 +1,19 @@
 import AllCourseCard from "./AllCourseCard";
 
 const CourseTab = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <div>
+        <h1 className="text-4xl text-center font-black">
+          Sorry no items available at the moment
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <>
-      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 mx-auto">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 py-10">
         {items.map((singleItem, idx) => (
           <AllCourseCard courseItem={singleItem} key={idx} />
         ))}
